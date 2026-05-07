a32f1bca5de7c73d32e335e97c54f3c262aa7314
Jay <jayparekhin02@gmail.com>
2026-05-07 15:29:49 +0530

    Added config server

--- src/main/java/com/ecom/ecomorder/EcomOrderApplication.java
+++ src/main/java/com/ecom/ecomorder/EcomOrderApplication.java
@@ -3,11 +3,16 @@ package com.ecom.ecomorder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EcomOrderApplication {
 
     static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(EcomOrderApplication.class, args);
     }
+
+
 
 }
